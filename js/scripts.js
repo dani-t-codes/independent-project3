ba480c776d06eb85d2d70ea14816e29f19c70b7b
Danielle Thompson <danithompson74@gmail.com>
2020-10-30 14:19:57 -0700

    Add exception to form entry to disclude any non-numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,12 @@ $(document).ready(function() {
   $("#roboForm").submit(function(event) {
     event.preventDefault();
       const roboFormInput = $("input#roboNumbers").val();
-      $("#roboResponse").prepend(roboFormInput + ", ");
+
+      if ("roboFormInput").includes(/\D/) {
+        $("#roboForm").alert("Try again! That's not a number.");
+      else if ("roboFormInput").includes(/\d/)
+        $("#roboResponse").prepend(roboFormInput + ", ");
+      }
     }); 
     
     
