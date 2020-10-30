42d9327f9412ba20ce85ef4febccfcbfdf8deb98
Danielle Thompson <danithompson74@gmail.com>
2020-10-30 15:14:41 -0700

    test else if statement for roboFormInput range

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,20 @@ $(document).ready(function() {
   $("#roboForm").submit(function(event) {
     event.preventDefault();
       const roboFormInput = $("input#roboNumbers").val();
-      const robogered = [];
+//      const robogered = [];
 
 //      if (robogered.includes(/\D/)) {
 //        $("#roboResponse").prompt("Not. A. Number. Try. A-gain.");
 //    } else if 
 //     if (roboFormInput.includes(/\d/)) 
-      if (roboFormInput <= 0 && ) {
-        $("#roboResponse").prompt("Not. A. Number. Try. A-gain.");
+      if (roboFormInput < '0' && roboFormInput > '50,000') {
+        $("#roboResponse").prompt("Not. In. Range. Try. A-gain.");
       }
+      else if (roboFormInput >= '0' && roboFormInput <='50,000') {
         $("#roboResponse").prepend(roboFormInput + ", ");
+        $("#roboResponse").show();
+        console.log(roboFormInput);
+      }
     
     
     }); 
