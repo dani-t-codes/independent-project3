ccf6985d7ac3f2f0784f2ac45570c5670041cf53
Danielle Thompson <danithompson74@gmail.com>
2020-10-30 16:09:37 -0700

    Fix show function by including correct section ID

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       const robogered = [roboFormInput];
       robogered.forEach(function(roboFormInput) {
         if (roboFormInput >= '0' && roboFormInput <='50,000') {
-          $("#roboResponse").show();
+          $(".goGoRobo").show();
           $("#roboResponse").prepend(roboFormInput + ", ");
           console.log(roboFormInput);
         }
