b78544951397459f4d717835073287f42cb2ea0a
Danielle Thompson <danithompson74@gmail.com>
2020-11-04 23:51:56 -0800

    Try to add an exception for if a value entered into beepBoop() is not a number to console log 'not a #' message, but fail

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
       roboFormInput.toString();
       let robogered = [];
       robogered.push(roboFormInput);
+      //I think the two lines of code below (10 & 11) would get added to the for loop on 31 with if/else if statements attached.
       $(".roboSays").show();
       $("#roboResponse").append(roboFormInput + ", ");
       
@@ -23,6 +24,8 @@ function beepBoop(number) {
     console.log("Will you be my neighbor?");
   } else if (number != 1 || 2 || 3) {
     console.log(number);
+  } else if (number === isNaN) {
+    console.log("Not. A. Number. Please. Try. Again.");
   }
 }
 
@@ -35,34 +38,27 @@ for (let i = 0; i < robogered.length; i ++) {
 };
 }); 
 });
-//   else if (formSequence.isNan() {
+
+
+
+
+
+//   if ("#roboResponse" === 1) {
+//     return true
+//   $("#roboResponse").append("Beep!" + ", ")
+//   } else if ('roboForm#roboNumbers' === 2) {
+//     console.log('roboForm#roboNumbers')
+//   }
+//   else if ('roboForm#roboNumbers' === 3) {
+  //     console.log("Will you be my neighbor?")
+//   } else if (formSequence.isNan() {
 //     return alert("Not. A. Number. Please. Try. Again.")
 //   };
 //   };
 // });
-
-
-
-  //   if ("#roboResponse" === 1) {
-  //     return true
-  // //        $("#roboResponse").append("Beep!" + ", ")
-  //   }
-  //   else if ('roboForm#roboNumbers' === 2) {
-  //     console.log('roboForm#roboNumbers')
-  //   }
-  //   else if ('roboForm#roboNumbers' === 3) {
-  //     console.log("Will you be my neighbor?")
-  //   }
-  // })
-  // }); 
-
-  
-  
-  //Business logic
-  
-  //    const roboSays = [];
-  
-  
+// })
+// }); 
+      
   
   
   
