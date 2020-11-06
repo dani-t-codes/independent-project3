afb92a20edbcabbd0069f41a575f5c96a263be76
Danielle Thompson <danithompson74@gmail.com>
2020-11-05 21:54:32 -0800

    Reorder UI and business logic, get .push working again

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,41 @@
-// User logic
+// User Interface
+
 $(document).ready(function() {
   $("#roboForm").submit(function(event) {
     event.preventDefault();
-      let roboFormInput = $("input#roboNumbers").val();
-      let robogered = [];
-      robogered.push.toString((roboFormInput));
-      //I think the two lines of code below (10 & 11) would get added to the for loop on 31 with if/else if statements attached.
-      $(".roboSays").show();
-      $("#roboResponse").text(roboFormInput);
-      
-      
- //Business Logic     
+    let roboFormInput = $("input#roboNumbers").val();
+    let robogered = [];
+
+    robogered.push(roboFormInput);
+    //I think the two lines of code below (10 & 11) would get added to the for loop on 31 with if/else if statements attached.
+    $(".roboSays").show();
+    $("#roboResponse").text(roboFormInput);
+  }); 
+});
+
+//Business Logic  
+function beepBoop(number) {
+//  let n = roboFormInput
+
+  robogered.map(i, n);
+  for (let i = 0; i < robogered.length; i ++) {
+    if (n === "1") {
+      console.log("0", "Beep!")
+    } else if (number === 2) {
+      console.log("0", "1", "Boop!");
+    } else if (number === 3) {
+      console.log("0", "1", "2", "Will you be my neighbor?");
+    } else if (number === isNaN) {
+      console.log("Not. A. Number. Please. Try. Again.");
+    }
+    };
+  } 
+    
 //const beepOutput = "Beep!";
 //let beepMe = function(beepOutput) 
 
-function beepBoop(number) {
-  if (number === 1) {
-    console.log("Beep!");
-  } else if (number === 2) {
-    console.log("Boop!");
-  } else if (number === 3) {
-    console.log("Will you be my neighbor?");
-  } else if (number != 1 || 2 || 3) {
-    console.log(number);
-  } else if (number === isNaN) {
-    console.log("Not. A. Number. Please. Try. Again.");
-  }
-}
 
-let userInput = false;
-for (let i = 0; i < robogered.length; i ++) {
-  if (robogered[i] === "1") {
-    userInput = true;
-    break;
-  };
-};
-}); 
-});
+
 
 
 
